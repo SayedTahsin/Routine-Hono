4374c98e38d2570912f4bf80904b4075bc35d06f
SayedTahsin <mail.tahsin99@gmail.com>
2024-10-22 23:32:31 +0600

    initialized cloudflare project
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

---
+++ schema.sql
@@ -0,0 +1,25 @@
+-- Create 'Notes' table
+CREATE TABLE IF NOT EXISTS Notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEXT NOT NULL,
+    mail TEXT NOT NULL,
+    color TEXT DEFAULT 'White'
+);
+
+-- Create 'Tasks' table
+CREATE TABLE IF NOT EXISTS Tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    text TEXT NOT NULL,
+    status BOOLEAN NOT NULL DEFAULT 0,
+    mail TEXT NOT NULL,
+    category TEXT NOT NULL
+);
+
+-- Create 'Users' table
+CREATE TABLE IF NOT EXISTS Users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    mail TEXT NOT NULL,
+    name TEXT NOT NULL,
+    completedTasks INTEGER DEFAULT 0,
+    totalTasks INTEGER DEFAULT 0
+);

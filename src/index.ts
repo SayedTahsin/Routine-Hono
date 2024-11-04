de1bbf036cb7167b77b898603f920caea4d47669
SayedTahsin <mail.tahsin99@gmail.com>
2024-11-04 21:43:07 +0600

    updated frontend dev url
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.use("*", async (c, next) => {
   await next();
-  c.header("Access-Control-Allow-Origin", "*"); // Set to your frontend's URL in production, e.g., 'https://yourfrontend.com'
+  c.header("Access-Control-Allow-Origin", "http://localhost:5173");
   c.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   c.header("Access-Control-Allow-Headers", "Content-Type");
 });
